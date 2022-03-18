28422c7ed14131dcac415cb225709170e937cefa
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-03-18 16:16:08 +0900

    a

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    char str1[12] = "Hello World";
+    char str2[10] = "Yokkaichi";
+    printf("%s ", str1);
+    printf("%s\n", str2);
+    
+    return 0;
+}

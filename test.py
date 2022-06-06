e8b02aac20f260d8bd4adebe118b5ccc7162252e
Shuji Onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-06-06 18:35:36 +0900

    nothing to comment

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import AkebonoMCAImporter
 
 importer = AkebonoMCAImporter.AkebonoMCAimporter('19940103')
 
-t = importer.output_mca_data('channel','Adata')
+t = importer.output_mca_data('channel','Channel')
 print(t)
 
-importer.attach_date('19950304')
-t = importer.output_mca_data('channel','Adata')
-print(t)
+#importer.attach_date('19950304')
+#t = importer.output_mca_data('channel','Adata')
+#print(t)

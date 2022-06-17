b0efaf94da4e8b8efec44074e32a1b0178498e3a
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-06-18 01:42:02 +0900

    change folder name from VLF_mca_cdf to VLF_mca

--- Akebono_orb_load.py
+++ Akebono_orb_load.py
@@ -1,10 +1,47 @@
-date = '19890301'
-file_name = date + '.orb'
-folder_path = './orbit/'
+import os as IMos
+import sys as IMsys
+import cdflib as IMcdflib
+import urllib.request as IMurllibreq
+import pytplot
+#Imported as above to avoid multiple imports, but not sure if this is necessary
 
-path = folder_path + file_name
+class Akebono_orb_load:
+    
+    def __init__(self,date):
+        self.input_date = date
+    
+    def attach_date(self,date):
+        self.input_date = date
+        
+    def mca(self,dirname = 'Akebono_orb_data'):
+        url_former_text = 'https://akebono-vlf.db.kanazawa-u.ac.jp/permalink.php?keyword=ak_h1_mca_'
+        url_later_text = '_v02.cdf'
 
-with open(path, mode = 'r') as f:
-    print(type(f))
-    lines = f.readlines()
-    print(len(lines))
+        url=url_former_text+self.input_date+url_later_text
+
+        pathname = './'+dirname+'/'
+
+        try:
+            IMos.mkdir(pathname)
+        except:
+            pass
+        
+        
+        save_name=pathname+'ak_h1_mca_'+self.input_date+'_v02.cdf'
+                  
+        if(IMos.path.isfile(save_name)==False):
+            
+            get_data = IMurllibreq.urlopen(url).read()
+
+            with open(save_name, mode="wb") as f:
+                f.write(get_data)
+
+        try:
+            cdf_file = IMcdflib.CDF(save_name)
+        except:
+            print('///////////////////////////ERROR/////////////////////////')
+            print("You can not get file or you can not open file \n")
+            IMsys.exit(1)
+        
+        tvars = pytplot.cdf_to_tplot(save_name)
+        return tvars

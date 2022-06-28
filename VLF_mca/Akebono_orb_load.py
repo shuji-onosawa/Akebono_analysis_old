f453389ae1442e34493ecc57418409b4c2008ded
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-06-29 02:20:07 +0900

    akebono orbit data (ILAT, MLT, ALT) plot

--- VLF_mca/Akebono_orb_load.py
+++ VLF_mca/Akebono_orb_load.py
@@ -3,6 +3,11 @@ import sys as IMsys
 import urllib.request as IMurllibreq
 import urllib
 import pytplot
+import numpy as np
+from pyspedas import time_double
+from pytplot import store_data
+
+
 #Imported as above to avoid multiple imports, but not sure if this is necessary
 
 class Akebono_orb_load:
@@ -47,5 +52,51 @@ class Akebono_orb_load:
             for i in range(len(datalist)):
                 datalist[i] = datalist[i].split()
         
-        datalist = datalist.T
-        return datalist
+        prefix = 'akb_orb_'
+        '''
+        datalist_header = [ 'PASS',
+                            'UT', 
+                            'ksc_azm(deg)', 'ksc_elv(deg)', 'ksc_dis(km)', 'ksc_ang(deg)', 
+                            'syo_azm(deg)', 'syo_elv(deg)', 'syo_dis(km)', 'syo_ang(deg)', 
+                            'pra_azm(deg)', 'pra_elv(deg)', 'pra_dis(km)', 'pra_ang(deg)', 
+                            'esr_azm(deg)', 'esr_elv(deg)', 'esr_dis(km)', 'esr_ang(deg)', 
+                            'GCLAT(deg)', 'GCLON(deg)', 
+                            'INV(deg)', 
+                            'FMLAT(deg)', 
+                            'MLAT(deg)', 
+                            'MLT(h)', 
+                            'Bmdl_X', 'Bmdl_Y', 'Bmdl_Z', 
+                            'GCLON_S/C(deg)', 'GCLAT_S/C(deg)',  
+                            'ALT(km)', 
+                            'LSUN', 
+                            's_Direc_x','s_Direc_y', 's_Direc_z', 
+                            's/c_pos_x', 's/c_pos_y', 's/c_pos_z', 
+                            's/c_vel(km/s)_x', 's/c_vel(km/s)_y','s/c_vel(km/s)_z' ]
+        '''
+        for data_list_index in range(1, len(datalist[1:])):
+            for data_index in range(len(datalist[data_list_index])):
+                datalist[data_list_index][data_index] = float(datalist[data_list_index][data_index])
+
+        del datalist[0]
+
+        data_array = np.array(datalist, dtype=float).T
+
+        YY = self.input_date[:2]
+
+        UT = data_array[1].tolist()
+        UT = [int(n) for n in UT]
+        UT = [str(n) for n in UT]
+        UT_time_double = []
+        for time_index in range(len(UT)):
+            time = UT[time_index]
+            time_string = YY + time[:2] + '/' + time[2:4] + '/' + time[4:6] + '/' + time[6:8] + ':' + time[8:10] + ':' + time[10:12]
+            #yyyymmdd.orb has UT data in the format of 'yymmddhhmmss'.
+            #To use pyspedas.time_double, change format from 'yymmddhhmmss' to 'yyyy/mm/dd/hh:mm:ss'
+            time_time_double = time_double(time_string)
+            UT_time_double.append(time_time_double)
+
+        store_data(prefix+'ILAT', data={'x': UT_time_double, 'y': data_array[20]})
+        store_data(prefix+'MLT', data={'x': UT_time_double, 'y': data_array[23]})
+        store_data(prefix+'ALT', data={'x': UT_time_double, 'y': data_array[29]})
+
+        return 
--- VLF_mca/tplot_test.py
+++ VLF_mca/tplot_test.py
@@ -5,11 +5,15 @@ from pytplot import tplot
 from pytplot import tlimit
 from pytplot import tplot_options
 import Akebono_mca_load
+import Akebono_orb_load
 import numpy as np
-importer = Akebono_mca_load.Akebono_mca_load('19900211')
-importer.mca()
 
-tplot_names = pytplot.tplot_names(True)
+importer1 = Akebono_mca_load.Akebono_mca_load('19900211')
+importer2 = Akebono_orb_load.Akebono_orb_load('19900211')
+importer1.mca()
+importer2.orb()
+
+tplot_names = pytplot.tplot_names()
 
 for i in range(4):
     tplot_variable = pytplot.get_data(tplot_names[i])
@@ -18,12 +22,9 @@ for i in range(4):
     tplot_variable_power = (10**(tplot_variable.y/10)) * (tplot_variable_0dB**2) / bandwidth 
     pytplot.store_data(tplot_names[i] +'_power', data={'x': tplot_variable.times, 'y': tplot_variable_power, 'v': tplot_variable.v})
 
-tlimit(['1990-02-11 18:00:00', '1990-02-11 18:14:00'])
+tlimit(['1990-02-11 01:00:00', '1990-02-11 03:00:00'])
 options('Emax', 'spec', 1)
-#options('Emax', 'ytitle', 'Freq [Hz]')
 options('Emax', 'ylog', 1)
-options('Emax', 'zrange', [40, 130])
-#options('Emax', 'ztitle', '$Emax [dB]$')
-options('Emax', 'char_size', 18)
+options('Emax', 'zrange', [40, 120])
 tplot_options('title', 'MCA data on Feb 11, 1990')
-tplot('Emax',save_png = 'savefig_test2')
+tplot('Emax', var_label = ['akb_orb_ILAT', 'akb_orb_MLT', 'akb_orb_ALT'], save_png = 'savefig_test2')

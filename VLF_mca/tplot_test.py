b484d926f085079990387dfa6f2bec13c0eaa278
Shuji Onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-06-29 17:39:27 +0900

    improve akb orb load

--- VLF_mca/tplot_test.py
+++ VLF_mca/tplot_test.py
@@ -8,8 +8,9 @@ import Akebono_mca_load
 import Akebono_orb_load
 import numpy as np
 
-importer1 = Akebono_mca_load.Akebono_mca_load('19900211')
-importer2 = Akebono_orb_load.Akebono_orb_load('19900211')
+date = '19910711'
+importer1 = Akebono_mca_load.Akebono_mca_load(date)
+importer2 = Akebono_orb_load.Akebono_orb_load(date)
 importer1.mca()
 importer2.orb()
 
@@ -19,12 +20,20 @@ for i in range(4):
     tplot_variable = pytplot.get_data(tplot_names[i])
     tplot_variable_0dB = 1e-6 #mV or pT
     bandwidth = tplot_variable.v * 0.3
-    tplot_variable_power = (10**(tplot_variable.y/10)) * (tplot_variable_0dB**2) / bandwidth 
-    pytplot.store_data(tplot_names[i] +'_power', data={'x': tplot_variable.times, 'y': tplot_variable_power, 'v': tplot_variable.v})
+    tplot_variable_amplitude = (10**(tplot_variable.y/20)) * (tplot_variable_0dB)  / np.sqrt(bandwidth)
+    pytplot.store_data(tplot_names[i] +'_power', data={'x': tplot_variable.times, 'y': tplot_variable_amplitude, 'v': tplot_variable.v})
 
-tlimit(['1990-02-11 01:00:00', '1990-02-11 03:00:00'])
-options('Emax', 'spec', 1)
-options('Emax', 'ylog', 1)
-options('Emax', 'zrange', [40, 120])
+
+tlimit(['1991-07-11 00:00:00', '1991-07-11 23:59:00'])
+options('Emax_power', 'spec', 1)
+options('Emax_power', 'ylog', 1)
+options('Emax_power', 'zlog', 1)
+options('Emax_power', 'zrange', [1e-5, 1])
+options('Emax_power', 'ztitle', '[mV/m/Hz^1/2]')
+options('akb_orb_ALT', 'ylabel', 'ALT [km]')
+options('akb_orb_MLT', 'ylabel', 'MLT [h]')
+options('akb_orb_ILAT', 'ylabel', 'ILAT [deg]')
 tplot_options('title', 'MCA data on Feb 11, 1990')
-tplot('Emax', var_label = ['akb_orb_ILAT', 'akb_orb_MLT', 'akb_orb_ALT'], save_png = 'savefig_test2')
+tplot('Emax_power', var_label = ['akb_orb_ALT', 'akb_orb_MLT', 'akb_orb_ILAT'], save_png = 'akb'+ date + '053300_test')
+
+print(bandwidth)

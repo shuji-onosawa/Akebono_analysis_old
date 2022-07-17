6be5a5fe5c5614b4c088a20f8620e875ecdc21bb
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-07-17 19:03:30 +0900

    nothing to say

--- VLF_mca/akebono_mca_plot.py
+++ VLF_mca/akebono_mca_plot.py
@@ -5,11 +5,11 @@ import numpy as np
 from load import mca, orb
 
 ILAT_min = 55
-start_day_string = '1994-01-01'
+start_day_string = '2000-01-01'
 start_day_time_double = pyspedas.time_double(start_day_string)
 seconds_per_day = 86400
 day_list = []
-for i in range(0, 367):
+for i in range(0, 368):
     time_double = start_day_time_double + i * seconds_per_day
     day_list.append(pyspedas.time_string(time_double, fmt='%Y-%m-%d %H:%M:%S'))
 

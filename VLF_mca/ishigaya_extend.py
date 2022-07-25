2f305e25287a0b85dde9f7cc3e9026d0054ba4bc
Shuji Onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-07-25 18:34:59 +0900

    isigaya extend

--- VLF_mca/ishigaya_extend.py
+++ VLF_mca/ishigaya_extend.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 ILAT_min = 55
 start_day_string = '1990-01-05'
 start_day_time_double = pyspedas.time_double(start_day_string)
-days = 181
+days = 180
 
 seconds_per_day = 86400
 day_list = []
@@ -17,6 +17,14 @@ for i in range(0, days+1):
     day_list.append(pyspedas.time_string(time_double, fmt='%Y-%m-%d %H:%M:%S'))
 
 Emax_pwr_10Hz_array = np.empty((0,0), float)
+Emax_pwr_176Hz_array = np.empty((0,0), float)
+Emax_pwr_316Hz_array = np.empty((0,0), float)
+Emax_pwr_562Hz_array = np.empty((0,0), float)
+
+E_to_B_176Hz_array = np.empty((0,0), float)
+E_to_B_316Hz_array = np.empty((0,0), float)
+E_to_B_562Hz_array = np.empty((0,0), float)
+
 Emax_pwr_mean_fcH_fLH_array = np.empty((0,0), float)
 
 for k in range(len(day_list)-1):
@@ -75,20 +83,93 @@ for k in range(len(day_list)-1):
     MLT = MLT.y
 
     Emax_pwr = get_data('Emax_Pwr')
+    Emax_amp = get_data('Emax_Amp')
+    Bmax_amp = get_data('Bmax_Amp')
+    
+    E_to_B = Emax_amp.y/Bmax_amp.y
+    
     index_tuple = np.where((ILAT > 55)&(10 < MLT)&(MLT < 14))
     Emax_in_cusp = Emax_pwr.y[index_tuple[0]]
+    E_to_B_in_cusp = E_to_B[index_tuple[0]]
             
     Emax_10Hz = Emax_in_cusp.T[2]
     Emax_176Hz_562Hz = Emax_in_cusp.T[7:10]
-            
+    E_to_B_176Hz_562Hz = E_to_B_in_cusp.T[7:10]
+    
     Index_over_th = [i for i, Emax_10Hz_var in enumerate(Emax_10Hz) if Emax_10Hz_var > 0.1]
     Emax_10Hz_over_th = Emax_10Hz[Index_over_th]
+    Emax_176Hz = Emax_176Hz_562Hz[0, Index_over_th]
+    Emax_316Hz = Emax_176Hz_562Hz[1, Index_over_th]
+    Emax_562Hz = Emax_176Hz_562Hz[2, Index_over_th]
+    
+    E_to_B_176Hz = E_to_B_176Hz_562Hz[0, Index_over_th]
+    E_to_B_316Hz = E_to_B_176Hz_562Hz[1, Index_over_th]
+    E_to_B_562Hz = E_to_B_176Hz_562Hz[2, Index_over_th]
+    
     Emax_mean_fch_fLH = np.nanmean(Emax_176Hz_562Hz[:, Index_over_th], axis = 0)
 
     Emax_pwr_10Hz_array = np.append(Emax_pwr_10Hz_array, Emax_10Hz_over_th)
+    Emax_pwr_176Hz_array = np.append(Emax_pwr_176Hz_array, Emax_176Hz)
+    Emax_pwr_316Hz_array = np.append(Emax_pwr_316Hz_array, Emax_316Hz)
+    Emax_pwr_562Hz_array = np.append(Emax_pwr_562Hz_array, Emax_562Hz)
+    
+    E_to_B_176Hz_array = np.append(E_to_B_176Hz_array, E_to_B_176Hz)
+    E_to_B_316Hz_array = np.append(E_to_B_316Hz_array, E_to_B_316Hz)
+    E_to_B_562Hz_array = np.append(E_to_B_562Hz_array, E_to_B_562Hz)
+    
     Emax_pwr_mean_fcH_fLH_array = np.append(Emax_pwr_mean_fcH_fLH_array, Emax_mean_fch_fLH)
-            
-plt.scatter(Emax_pwr_10Hz_array, Emax_pwr_mean_fcH_fLH_array)
+
+plt.figure()
+plt.scatter(Emax_pwr_10Hz_array, Emax_pwr_mean_fcH_fLH_array, s=1)
 plt.xlabel('Epwr @10 Hz \n [(mV/m)^2/Hz] ')
-plt.ylabel('Epwr average in 316-562 Hz \n [(mV/m)^2/Hz]')
+plt.ylabel('Epwr average in 176-562 Hz \n [(mV/m)^2/Hz]')
+plt.savefig('10Hz_vs_176-562Hz_ave')
+plt.show()
+
+
+plt.figure()
+plt.scatter(Emax_pwr_10Hz_array, Emax_pwr_176Hz_array, s=1)
+plt.xlabel('Epwr @10 Hz \n [(mV/m)^2/Hz] ')
+plt.ylabel('Epwr @176 Hz \n [(mV/m)^2/Hz]')
+plt.savefig('E-10Hz_vs_E-176Hz')
+plt.show()
+
+
+plt.figure()
+plt.scatter(Emax_pwr_10Hz_array, Emax_pwr_316Hz_array, s=1)
+plt.xlabel('Epwr @10 Hz \n [(mV/m)^2/Hz] ')
+plt.ylabel('Epwr @316 Hz \n [(mV/m)^2/Hz]')
+plt.savefig('E-10Hz_vs_E-316Hz')
+plt.show()
+
+
+plt.figure()
+plt.scatter(Emax_pwr_10Hz_array, Emax_pwr_562Hz_array, s=1)
+plt.xlabel('Epwr @10 Hz \n [(mV/m)^2/Hz] ')
+plt.ylabel('Epwr @562 Hz \n [(mV/m)^2/Hz]')
+plt.savefig('E-10Hz_vs_E-562Hz')
+plt.show()
+
+
+plt.figure()
+plt.scatter(Emax_pwr_10Hz_array, E_to_B_176Hz_array, s=1)
+plt.xlabel('Epwr @10 Hz \n [(mV/m)^2/Hz] ')
+plt.ylabel('E/B @176 Hz \n [m/s]')
+plt.savefig('E-10Hz_vs_EtoB-176Hz')
+plt.show()
+
+
+plt.figure()
+plt.scatter(Emax_pwr_10Hz_array, E_to_B_316Hz_array, s=1)
+plt.xlabel('Epwr @10 Hz \n [(mV/m)^2/Hz] ')
+plt.ylabel('E/B @316 Hz \n [m/s]')
+plt.savefig('E-10Hz_vs_EtoB_316Hz')
+plt.show()
+
+
+plt.figure()
+plt.scatter(Emax_pwr_10Hz_array, E_to_B_562Hz_array, s=1)
+plt.xlabel('Epwr @10 Hz \n [(mV/m)^2/Hz] ')
+plt.ylabel('E/B @562 Hz \n [m/s]')
+plt.savefig('E-10Hz_vs_EtoB_562Hz')
 plt.show()

7fdf8cdb635ed3aa2e515a06a373c870a5adc4f3
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-08-30 16:18:39 +0900

    mca plot 2003

--- VLF_mca/akebono_mca_plot.py
+++ VLF_mca/akebono_mca_plot.py
@@ -12,12 +12,12 @@ channels = ["3.16 Hz", "5.62 Hz", "10 Hz", "17.6 Hz",
 surfix = 'Pwr'
 
 ILAT_min = 55
-start_day_string = '1991-06-09'
+start_day_string = '2003-12-01'
 start_day_time_double = pyspedas.time_double(start_day_string)
 
 seconds_per_day = 86400
 day_list = []
-for i in range(0, 2):
+for i in range(0, 32):
     time_double = start_day_time_double + i * seconds_per_day
     day_list.append(pyspedas.time_string(time_double, fmt='%Y-%m-%d %H:%M:%S'))
 

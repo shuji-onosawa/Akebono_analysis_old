aae769244e9e2aef91c0851c976e8ccb06b30ef9
Shuji Onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2022-09-05 17:38:38 +0900

    how to read the document of MCA postgap data (memo.ipynb)

---
+++ VLF_mca/Akebono_ELF_data/elf/ak_h1_elf_19900206_v03.cdf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d31b457cc7d004550a4c1f1eca516127ac21628bdffc62011030d0fe0fd0761
+size 404
--- VLF_mca/test.py
+++ VLF_mca/test.py
@@ -1,14 +1,5 @@
-import pytplot, load
-from collections import OrderedDict
-load.mca(['1990-01-01', '1990-01-02'])
-
-a = pytplot.get_data('Emax')
-od = OrderedDict()
-
-od['k1'] = 1
-od['k2'] = 2
-od['k3'] = 3
-
-print(od)
+import pyspedas, pytplot, load
+a = load.mca(['1990-02-01', '1990-02-02'])
+b =pytplot.data_quants['Emax'].CDF['VATT']
 
 print('test')

a10e318bedeefdcd582882a4925fcbb331311df9
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-03-30 11:38:23 +0900

    Add plot of MCA spectrogram and Emax/Bmax power for February 25, 1990

--- VLF_mca/plot_spectram.py
+++ VLF_mca/plot_spectram.py
@@ -3,16 +3,21 @@ import numpy as np
 from get_antenna_angle import unit_vector, angle_between_vectors
 import cdflib
 from load import mca_h1cdf_dB_to_absolute
+import matplotlib.pyplot as plt
+import datetime as dt
 
+date = '19900225'
+trange = [date[0:4]+'-'+date[4:6]+'-'+date[6:8]+' 12:20:00',
+          date[0:4]+'-'+date[4:6]+'-'+date[6:8]+' 12:30:00']
 Ey_antenna_vector = np.array([-np.sin(np.deg2rad(35)),
                               np.cos(np.deg2rad(35)),
                               0])
 sBy_antenna_vector = np.array([0.0, -1.0, 0.0])
 
-cdf_name = './akebono_data/mgf/ak_h0_mgf_19900225_v01.cdf'
+cdf_name = './akebono_data/mgf/ak_h0_mgf_'+date+'_v01.cdf'
 mgf_xary = cdflib.cdf_to_xarray(cdf_name)
 
-B0_epoch = mgf_xary['Epoch'].data
+B0_epoch = mgf_xary['Epoch'].data / 1000
 B0_ary = mgf_xary['B0_spin'].data
 
 angle_btwn_B0_Ey = np.empty(int((B0_ary.shape[0]+1)/2-1))
@@ -22,11 +27,15 @@ angle_btwn_B0_antenna_epoch = np.empty(int((B0_ary.shape[0]+1)/2-1))
 for i in range(int((B0_ary.shape[0]+1)/2-1)):
     # 1秒値データを想定しています。
     if B0_epoch[2*i] == 0.0:
-        angle_btwn_B0_antenna_epoch[i] = np.nan
+        angle_btwn_B0_antenna_epoch[i] = 0.0
         angle_btwn_B0_Ey[i] = np.nan
         angle_btwn_B0_sBy[i] = np.nan
     else:
-        angle_btwn_B0_antenna_epoch[i] = B0_epoch[2*1]
+        # mgfのデータ構造
+        # epoch: 長さ172799の1次元配列 (1日/0.5秒=172800)
+        # b0_spin: 172799*3*16の3次元配列
+        # 1秒値にするために2つの連続する3*16の配列の最後と最初の値をとってきて平均
+        angle_btwn_B0_antenna_epoch[i] = B0_epoch[2*i]
         B0_vector = np.array([(B0_ary[2*i][0][15]+B0_ary[2*i+1][0][0])/2,
                               (B0_ary[2*i][1][15]+B0_ary[2*i+1][1][0])/2,
                               (B0_ary[2*i][2][15]+B0_ary[2*i+1][2][0])/2])
@@ -49,9 +58,8 @@ pytplot.options(name='angle_btwn_B0_sBy',
                           'yrange': [-180, 180]})
 
 
-pytplot.cdf_to_tplot('./akebono_data/vlf/mca/h1/ave1s/ak_h1_mca_19900225_v02.cdf')
+pytplot.cdf_to_tplot('./akebono_data/vlf/mca/h1/ave8s/ak_h1_mca_'+date+'_v02.cdf')
 mca_h1cdf_dB_to_absolute(spec_type='pwr')
-
-pytplot.tlimit(['1990-2-25 12:20:00', '1990-2-25 12:30:00'])
-pytplot.tplot(['Emax_pwr', 'angle_btwn_B0_Ey',
-               'Bmax_pwr', 'angle_btwn_B0_sBy'])
+Emax_tvar = pytplot.get_data('Emax')
+pytplot.tlimit(trange)
+pytplot.tplot(['Emax_pwr', 'Bmax_pwr'], save_png='./plots/mca_spin_modulation/mca_spec_ave8s_wide')
